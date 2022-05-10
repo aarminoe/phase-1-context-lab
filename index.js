9abc4e6ba1cb9e6b6b4548a5b216bd2156a58271
Aaron Noe <aarminoe@gmail.com>
2022-05-09 19:15:22 -0500

    commit tree b8c2895a427eeea33081b4a451a77d8d1372f716

--- index.js
+++ index.js
@@ -1,5 +1,80 @@
 /* Your Code Here */
 
+function createEmployeeRecord(empArray) {
+    return {
+        'firstName':  empArray[0],
+        'familyName': empArray[1],
+        'title': empArray[2],
+        'payPerHour': parseInt(empArray[3]),
+        'timeInEvents': [],
+        'timeOutEvents': [],
+    }
+}
+
+function createEmployeeRecords (empArray) {
+    let arrayOfRecords = []
+    empArray.forEach(array => {
+        let empObj = createEmployeeRecord(array)
+        arrayOfRecords.push(empObj)
+    })
+    return arrayOfRecords
+}
+
+function createTimeInEvent(dateStamp) {
+    let [date,hour] = dateStamp.split(' ')
+    this.timeInEvents.push({
+        'type': 'TimeIn',
+        'hour': parseInt(hour),
+        'date': date
+    })
+    return this  
+}
+
+function createTimeOutEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(' ')
+    this.timeOutEvents.push({
+        'type': 'TimeOut',
+        'hour': parseInt(hour),
+        'date': date
+    })
+    return this
+}
+
+function hoursWorkedOnDate(dateStamp){
+    let timeInKeys = this.timeInEvents.find((e) => {
+        return e.date === dateStamp
+    })
+    let timeOutKeys = this.timeOutEvents.find((e => {
+        return e.date === dateStamp
+    }))
+    return (timeOutKeys.hour - timeInKeys.hour) /100
+}
+
+function wagesEarnedOnDate(dateStamp) {
+    return hoursWorkedOnDate.call(this, dateStamp) * this.payPerHour
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find((array) => {
+        if (array.firstName === firstName) {
+            return array
+        }
+        else {
+            return undefined
+        }
+    })
+}
+
+function calculatePayroll(empArray) {
+    let totalTime = 0
+    empArray.forEach(empRecord => {
+        totalTime += allWagesFor.call(empRecord)
+    })
+    return totalTime
+}
+
+
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
